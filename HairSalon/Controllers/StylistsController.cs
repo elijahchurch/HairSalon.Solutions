3e3563274940d26b9c92a54034cadd1476d152aa
Jason E. Church <elijahchurch@gmail.com>
2023-07-28 10:10:24 -0700

    Set up StylistsController and create index route

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+    public Class StylistsController : Controller
+    {
+        private readonly HairSalonContext _db;
+        public StylistsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Stylist> stylistsModel = _db.Stylists.ToList();
+            return View(stylistsModel);
+        }
+    }
+}
+
